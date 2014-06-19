36a5c51a042af6db374554651eb669d41ff090ab
Stephen Weigel <stephen@stephenweigel.com>
2014-06-19 12:35:07 -0400

    Move conversion to a RomanNumeral class.
    This prevents the Integer class from being polluted with logic related to
    Roman Numerals. The to_roman method then simply calls a conversion method
    on the Roman Numeral class.

--- roman.rb
+++ roman.rb
@@ -1,18 +1,27 @@
-class Integer
-  def to_roman(number = self, numeral = '')
+class RomanNumeral
+  def initialize(number)
     fail ArgumentError unless number.is_a?(Integer)
-    return 'There is no Roman Numeral for 0' if self == 0
-    return numeral if number == 0
-    numeral_conversion.keys.each do |divisor|
-      quotient, modulus = number.divmod(divisor)
-      numeral << numeral_conversion[divisor] * quotient
-      return to_roman(modulus, numeral) if quotient > 0
-    end
+    @number = number
+    @numeral = ''
+  end
+
+  def convert_from_number
+    return 'There is no Roman Numeral for 0' if @number == 0
+    numeral_conversion
+    @numeral
   end
 
   private
 
-  def numeral_conversion
+  def numeral_conversion(number = @number)
+    numeral_conversion_values.keys.each do |divisor|
+      result, modulus = number.divmod(divisor)
+      @numeral << numeral_conversion_values[divisor] * result
+      return numeral_conversion(modulus) if result > 0
+    end
+  end
+
+  def numeral_conversion_values
     {
       1000 => 'M',
       900 => 'CM',
@@ -28,5 +37,11 @@ class Integer
       4 => 'IV',
       1 => 'I'
     }
+  end
+end
+
+class Integer
+  def to_roman
+    RomanNumeral.new(self).convert_from_number
   end
 end
